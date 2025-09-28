9ad9128b44ef7ec4d8e19e2aeed6206f0029155b
Okala Fortune <okalafortune146@gmail.com>
2025-09-28 21:59:57 +0100

    added an update in variables.js

--- variables.js
+++ variables.js
@@ -4,8 +4,12 @@ const name = "Okala Fortune";
 // let and var (can be reassigned)
 //var can be redeclared and reassigned within the same scope
 var y = 50; 
-y = 10; // reassignmentallowed
+y = 10; // reassignment allowed
 
 var y = 30; // redeclaration also allowed
 
 console.log(y); // would give us 30
+
+// let can be reassigned but can't be redeclared
+let y = 50;
+y = 40; //reassignment allowed
